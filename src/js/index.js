988afa812c7597581201fed9d2ad0bb562d4257d
Gustv87 <tavomartinez87@gmail.com>
2021-04-29 12:00:09 -0600

    Se refacterizo el js

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,14 @@
-function btnNLS() {
-  window.open('https://nls.app.sostruckingsoftware.com/account/login');
+function openLogin(empresa) {
+  window.open(url[empresa]);
 }
 
-function btnCTS() {
-  window.open('https://citius.app.sostruckingsoftware.com/account/login');
-}
-
-function btnMCI() {
-  window.open('http://mciexpress.sostruckingsoftware.com:81/account/login');
-}
-
-function btnGBS() {
-  window.open('https://gables.sostruckingsoftware.com:8045/account/login');
-}
-
-function btnAREL() {
-  window.open('https://arel.sostruckingsoftware.com:8045/account/login');
-}
-
-function btnFDS() {
-  window.open('https://fds.app.sostruckingsoftware.com:8043/');
-}
-
-function btnAQI() {
-  window.open('http://aqilogistic.sostruckingsoftware.com:8038/account/login');
-}
-
-function btnAE() {
-  window.open('https://avalanche.app.sostruckingsoftware.com/');
-}
+const url = {
+  nls: 'https://nls.app.sostruckingsoftware.com/account/login',
+  citius: 'https://citius.app.sostruckingsoftware.com/account/login',
+  mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
+  gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
+  arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
+  fds: 'https://fds.app.sostruckingsoftware.com:8043/',
+  aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
+  ae: 'https://avalanche.app.sostruckingsoftware.com/',
+};
