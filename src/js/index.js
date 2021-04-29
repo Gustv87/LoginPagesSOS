31a5c3e9bc820a42053bd13294851a8326d043fa
Gustv87 <tavomartinez87@gmail.com>
2021-04-28 21:00:35 -0600

    Se creo una pagina web para el manejo de logins de SOS

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+function btnNLS() {
+  window.open('https://nls.app.sostruckingsoftware.com/account/login');
+}
+
+function btnCTS() {
+  window.open('https://citius.app.sostruckingsoftware.com/account/login');
+}
+
+function btnMCI() {
+  window.open('http://mciexpress.sostruckingsoftware.com:81/account/login');
+}
+
+function btnGBS() {
+  window.open('https://gables.sostruckingsoftware.com:8045/account/login');
+}
+
+function btnAREL() {
+  window.open('https://arel.sostruckingsoftware.com:8045/account/login');
+}
+
+function btnFDS() {
+  window.open('https://fds.app.sostruckingsoftware.com:8043/');
+}
+
+function btnAQI() {
+  window.open('http://aqilogistic.sostruckingsoftware.com:8038/account/login');
+}
+
+function btnAE() {
+  window.open('https://avalanche.app.sostruckingsoftware.com/');
+}
