128120fef81519da7c0539d40c09fd758b9b6c90
Gustv87 <tavomartinez87@gmail.com>
2021-04-30 15:29:24 -0600

    Se agrego PC Express

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,8 @@ const url = {
   mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
   gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
   arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
-  fds: 'https://fds.app.sostruckingsoftware.com:8043/',
+  fds: 'https://fds.app.sostruckingsoftware.com:8043/account/login',
   aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
-  ae: 'https://avalanche.app.sostruckingsoftware.com/',
+  ae: 'https://avalanche.app.sostruckingsoftware.com/account/login',
+  pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login'
 };
