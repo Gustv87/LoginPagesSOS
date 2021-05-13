7d4c1da469ba3dc964ef6b9f124b416b1bc7031d
Gustv87 <tavomartinez87@gmail.com>
2021-05-13 09:40:22 -0600

    correccion del link de fds

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const url = {
   mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
   gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
   arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
-  fds: 'https://fds.app.sostruckingsoftware.com:8043/account/login',
+  fds: 'https://fds.sostruckingsoftware.com:8043/',
   aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
   ae: 'https://avalanche.app.sostruckingsoftware.com/account/login',
   pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
