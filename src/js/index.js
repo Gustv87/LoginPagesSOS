4cf23391965d1d3166fd6d14cf6eed54cfbd6d05
Gustv87 <tavomartinez87@gmail.com>
2021-05-18 15:11:16 -0600

    Se cambiaron las ulr de NLS, Citius y Avalache

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,16 @@ function openLogin(empresa) {
 }
 
 const url = {
-  nls: 'https://nls.app.sostruckingsoftware.com/account/login',
-  citius: 'https://citius.app.sostruckingsoftware.com/account/login',
+  nls: 'https://nls.sostruckingsoftware.com/account/login',
+  citius: 'https://citius.sostruckingsoftware.com/account/login',
   mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
   gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
   arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
   fds: 'https://fds.sostruckingsoftware.com:8043/',
   aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
-  ae: 'https://avalanche.app.sostruckingsoftware.com/account/login',
+  ae: 'https://avalanche02.sostruckingsoftware.com/account/login',
   pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
-  akt: 'https://akt.sostruckingsoftware.com/account/login'
+  akt: 'https://akt.sostruckingsoftware.com/account/login',
 };
+
 
