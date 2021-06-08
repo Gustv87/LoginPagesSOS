366746679eba63003b95450471d7331f8c403a3d
Gustv87 <tavomartinez87@gmail.com>
2021-06-08 12:10:40 -0600

    Se realizo cambio en la url de FDS

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const url = {
   mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
   gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
   arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
-  fds: 'https://fds.sostruckingsoftware.com:8043/',
+  fds: 'https://fds.sostruckingsoftware.com/',
   aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
   ae: 'https://avalanche02.sostruckingsoftware.com/account/login',
   pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
