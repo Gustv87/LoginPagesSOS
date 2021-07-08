411e99221e5e31b927ce3462392dabc2d18ac62f
Gustv87 <tavomartinez87@gmail.com>
2021-07-08 12:00:39 -0600

    Se agregaron los links de R & E y Andoba y se modifico el link de FDS

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,13 @@ const url = {
   mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
   gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
   arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
-  fds: 'https://fds.sostruckingsoftware.com/',
+  fds: 'https://fds.sostruckingsoftware.com/account/login',
   aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
   ae: 'https://avalanche02.sostruckingsoftware.com/account/login',
   pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
   akt: 'https://akt.sostruckingsoftware.com/account/login',
+  andoba:'https://andoba.sostruckingsoftware.com/account/login',
+  re: ' https://re-test.sostruckingsoftware.com:8043/account/login',
 };
 
 
