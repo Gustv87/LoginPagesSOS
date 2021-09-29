f2ab7576503df273895fcb429960cff3ff543779
Gustavo Martinez <tavomartinez87@gmail.com>
2021-09-28 21:50:12 -0600

    Se agregaron H&D, A&V y BLT

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,26 @@
 function openLogin(empresa) {
-  window.open(url[empresa]);
+	window.open(url[empresa]);
 }
 
 const url = {
-  nls: 'https://nls.sostruckingsoftware.com/account/login',
-  citius: 'https://citius.sostruckingsoftware.com/account/login',
-  mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
-  gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
-  arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
-  fds: 'https://fds.sostruckingsoftware.com/account/login',
-  aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
-  ae: 'https://avalanche02.sostruckingsoftware.com/account/login',
-  pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
-  akt: 'https://akt.sostruckingsoftware.com/account/login',
-  andoba:'https://andoba.sostruckingsoftware.com/account/login',
-  re: ' https://re-test.sostruckingsoftware.com:8043/account/login',
+	nls: 'https://nls.sostruckingsoftware.com/account/login',
+	citius: 'https://citius.sostruckingsoftware.com/account/login',
+	mci: 'http://mciexpress.sostruckingsoftware.com:81/account/login',
+	gbs: 'https://gables.sostruckingsoftware.com:8045/account/login',
+	arel: 'https://arel.sostruckingsoftware.com:8045/account/login',
+	fds: 'https://fds.sostruckingsoftware.com/account/login',
+	aqi: 'http://aqilogistic.sostruckingsoftware.com:8038/account/login',
+	ae: 'https://avalanche02.sostruckingsoftware.com/account/login',
+	pc: 'http://pcexpress.app.sostruckingsoftware.com/account/login',
+	akt: 'https://akt.sostruckingsoftware.com/account/login',
+	andoba: 'https://andoba.sostruckingsoftware.com/account/login',
+	re: ' https://re-test.sostruckingsoftware.com:8043/account/login',
+	av: 'https://av.sostruckingsoftware.com/',
+	hd: 'https://hd.sostruckingsoftware.com/',
+	blueLine: 'https://blueline.sostruckingsoftware.com/',
 };
 
+// for (const empresa in url) {
+//   console.log(`${empresa}: ${url[empresa]}`);
 
+// }
